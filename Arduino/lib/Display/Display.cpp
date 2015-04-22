ea3bd38738d14094c640c2cf425cc71f1ef065ba
BeppeH <beppe@birdwing.se>
2015-04-22 13:12:21 +0200

    Alla bibliotek
    Ny mapp för alla bibliotek och klasser.

---
+++ Arduino/lib/Display/Display.cpp
@@ -0,0 +1,25 @@
+/*
+   Library to print a string to a 20x4 Display and a Display-controller with an address of 0x27
+*/
+
+#include "Arduino.h"
+#include "Display.h"
+
+Display::Display() 
+{
+ 	
+}
+
+void Display::Write(String str)
+{
+	lcd.print(str);
+}
+
+void Display::Begin()
+{
+	lcd.init(); 
+	lcd.backlight();
+	lcd.setCursor(0, 0); 
+	lcd.clear(); 
+}
+
