19ac9a6303b80d2bc206192e2bb3ca168d490003
BeppeH <beppe@birdwing.se>
2015-04-24 11:51:55 +0200

    Flyttat på biblioteken

--- Arduino/Bryggverket/Bryggverket.ino
+++ Arduino/Bryggverket/Bryggverket.ino
@@ -1,16 +1,17 @@
-#include <MashSchedule.h>
+#include "MashSchedule.h"
 #include <Time.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <Wire.h>
 #include <LiquidCrystal.h>
-#include <Display.h>
-#include <SerialString.h>
+#include "Display.h"
+#include "SerialString.h"
 
 MashSchedule test1;
 
 void setup()
 {
+  
   	Serial.begin(9600);
 	test1.Default();
 }
