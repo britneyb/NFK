bfc80265ff6b4512ddcd1358d4f2ec1791b007b6
BeppeH <beppe@birdwing.se>
2015-04-24 13:13:48 +0200

    Tagit bort temporära och onödiga filer
    Nu ligger alla klasser i samma mapp som bryggverket.ino och alla
    bibliotek i mappen lib.

--- Arduino/Demo1/Sketch1/Debug/new.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Sebastian\AppData\Local\V.Micro\Arduino\Builds\Sketch1\uno\new.cpp.o: \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\new.cpp
--- Arduino/Sketch2/Sketch2/Debug/abi.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Sevag\AppData\Local\V.Micro\Arduino\Builds\Sketch2\uno\abi.cpp.o: \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\abi.cpp
