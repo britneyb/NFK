6e1f1242e9bb0ded235400d7419a6a1cfdb2cb7b
Sansan91 <zarnor91@gmail.com>
2015-05-19 15:36:45 +0200

    Gjort så scriptet lyssnar på seriell och lägger in värden i db

--- PythonScript/PythonPi.py
+++ PythonScript/PythonPi.py
@@ -3,6 +3,8 @@ import serial
 import time
 import socket
 import os
+import re
+import sqlite3
 
 
 gw = os.popen("ip -4 route show default").read().split()
@@ -12,15 +14,20 @@ ipaddr = s.getsockname()[0]
 
 print ("IP:", ipaddr)
 
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1) # Establish the connection on a specific port
 
+ser.write(("ip,IP:"+ipaddr).encode('utf-8'))
 
+con = sqlite3.connect('sqltemptime.db')
+c = con.cursor();
 
-
-ser = serial.Serial('/dev/ttyACM0', 9600) # Establish the connection on a specific port
-
-
-ser.write("ip,"+ipaddr)
-
-
-
+while ser.isOpen():
+	data = (ser.readline()).decode('utf-8')
+	if data != "":
+		match = re.match(r'\w+,\d+,\d+,\d+', data)
+		#print(match)
+		match2 = re.search(r'(\w+),(\d+),(\d+),(\d+)', match.group(0))
+		c.execute("INSERT INTO timeTemp (time, temp, id) VALUES (?, ?, ?)", ("{}".format(match2.group(3)), "{}".format(match2.group(4)), "{}".format(match2.group(2))))
+		con.commit();
+		print(match2.group(1)+":"+match2.group(2)+":"+match2.group(3)+":"+match2.group(4))
 
