b1c7fcf353d492779c53b0341872b112f9668ec0
BeppeH <beppe@birdwing.se>
2015-05-20 14:14:42 +0200

    Rensat upp i mappen arduino

--- Arduino/testingsomeshit/testingsomeshit/Debug/DS1307RTC/DS1307RTC.cpp.d
+++
@@ -1,9 +0,0 @@
-C:\Users\Sevag\AppData\Local\V.Micro\Arduino\Builds\testingsomeshit\yun\DS1307RTC\DS1307RTC.cpp.o: \
- C:\Program\ Files\ (x86)\Arduino\libraries\DS1307RTC\DS1307RTC.cpp \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire/Wire.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\DS1307RTC\DS1307RTC.h \
- C:\Program\ Files\ (x86)\Arduino\libraries\Time/Time.h
--- Arduino/testingsomeshit/testingsomeshit/Debug/WInterrupts.c.d
+++
@@ -1,7 +0,0 @@
-C:\Users\Sevag\AppData\Local\V.Micro\Arduino\Builds\testingsomeshit\yun\WInterrupts.c.o: \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\WInterrupts.c \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\wiring_private.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\Arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\binary.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\yun/pins_arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\yun/../leonardo/pins_arduino.h
